551863d732692cee0bcc33f4c635723158549a0b
roai20 <ys.kim@roai.im>
2025-04-28 11:10:44 +0900

    update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/roai20/YS/github/test_repo/src
+# Build directory: /home/roai20/YS/github/test_repo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("Gazebo_cable")
---
+++ build/Gazbo_cable/CMakeFiles/cable_vel_control_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/roai20/YS/github/test_repo/devel/lib/libcable_vel_control_plugin.pdb"
+  "/home/roai20/YS/github/test_repo/devel/lib/libcable_vel_control_plugin.so"
+  "CMakeFiles/cable_vel_control_plugin.dir/src/cable_vel_control_plugin.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cable_vel_control_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
