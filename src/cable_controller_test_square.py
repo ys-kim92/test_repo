9af09d01cc22d339ced60c6f7a2b99d5575b4474
roai20 <ys.kim@roai.im>
2025-04-28 10:11:31 +0900

    test2

--- src/cable_controller_test_square.py
+++ src/cable_controller_test_square.py
@@ -12,7 +12,7 @@ def move_end_sphere():
     # Create a publisher to publish the target position
     pub = rospy.Publisher('cable_target_position', Point, queue_size=10)
     target_position = Point()
-    offset = 0.5
+    offset = 0.5 # 주석 추가
 
     # Go to the first target position
 
